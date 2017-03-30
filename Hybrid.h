0970817623be9602dadc7e253ebf48c7c5db8860
TacticalSarcasm <jakerrocks75@yahoo.com>
2017-03-30 12:45:57 -0500

    Basic Outline

--- Hybrid.h
+++ Hybrid.h
@@ -44,6 +44,76 @@ Hybrid<T>::~Hybrid()
 //simply comment the first implementation out when working on the second implementation
 //use the getKey method to dequeue/remove
 
+template < class T >
+bool Hybrid<T>::isEmpty()
+{
+	int sz = sldl->size();
+	if(sz == 0)
+	{
+		return true;
+	}
+	else
+		return false;
+}
+
+template < class T >
+void Hybrid<T>::enqueue(T* item)
+{
+    DoubleNode<T>* node = new DoubleNode<T>(item);
+
+    if (isEmpty()) 
+    {
+      node->setNext(node);
+    }
+	/*
+    else 
+    {
+      node->setNext(tail->getNext());
+      tail->setNext(node);
+    }  
+
+    tail = node;  //new node is at back
+	*/
+}
+
+template < class T >
+T* Hybrid<T>::dequeue()
+{
+	/*
+	
+    T* item = NULL;
+
+    if (!isEmpty()) 
+    {
+      // queue is not empty; remove front
+      DoubleNode<T>* head = tail->getNext();
+      item = head->getItem();
+
+      if (head == tail) 
+      {  
+        // one node in queue
+        tail->setNext(NULL); 
+        tail = NULL;        
+      }
+      else 
+      {
+        tail->setNext(head->getNext());
+      }  
+
+      delete head;
+      sze--;
+    }
+
+    return item;
+	*/
+}
+
+template < class T >
+ListDoublyLinkedIterator<T>* Hybrid<T>::iterator()
+{
+   return sldl->iterator();
+}
+
 
 
 
